909000d36b9edb80b5272dfda1c9cf24d4c6244c
Javier Cabezas <javiercp69@educastur.es>
2022-09-27 08:29:06 +0200

    Primer commit

---
+++ UD1/ejercicio1/script.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Nombre");
+let apellidos = prompt("Apellidos");
+let altura = parseInt( prompt("Altura"));
+let talla = parseInt( prompt("Talla"));
+console.log("nombre " + typeof(nombre) + " apellidos " + typeof(apellidos) + " altura " + typeof(altura) + " talla " + typeof(talla))
+console.log(nombre + "\n" +  apellidos)
+console.log("suma " +(altura + talla))
---
+++ UD1/holaMundo2.js
@@ -0,0 +1,11 @@
+       
+       // Programa para probar sintaxis basica
+
+       // alert("Hola mundo");
+       // console.log("Ahora saludo a la consola");
+
+     /*    Comentario de
+        varias lineas */
+
+       var numero="25";
+       console.log
