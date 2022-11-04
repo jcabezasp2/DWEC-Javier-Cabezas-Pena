ca164d4d515edaf345eb6fb48b68d8b38209cf46
Javier Cabezas <javiercp69@educastur.es>
2022-11-04 10:41:36 +0100

    Todo terminado

---
+++ UD2/ACT3/js/principal.js
@@ -0,0 +1,11 @@
+
+let central = new centralMedidas();
+console.log(central.insertaAleatorio("Santander")) ;
+central.insertaAleatorio("Oviedo");
+central.insertaAleatorio("Madrid");
+central.insertaAleatorio("Madrid");
+central.toConsole();
+console.log(central.mediaMedidas("Oviedo"));
+console.log(central.mediaMedidasTotal());
+central.eliminaCiudad("Oviedo");
+central.toConsole();
